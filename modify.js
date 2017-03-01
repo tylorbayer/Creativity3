176eb9462a01d95220bcf5c6490a94d7dc6fd940
Tylor Bayer <tylor.bayer@gmail.com>
2017-02-28 18:13:25 -0700

    final editable

--- modify.js
+++ modify.js
@@ -8,8 +8,7 @@ function InlineEditorController($scope){
 	// variables. Here we set some default values:
 
 	$scope.showtooltip = false;
-	$scope.value = 'Edit me.';
-  $scope.value1 = 'Edit me.';
+	$scope.value = 'Enter Quick Personal Sumary Here.';
 
 	// Some helper functions that will be
 	// available in the angular declarations
@@ -38,7 +37,33 @@ function InlineEditorController1($scope){
 	// variables. Here we set some default values:
 
 	$scope.showtooltip = false;
-  $scope.value1 = 'Edit me.';
+  $scope.value1 = 'Enter About Me Info Here.';
+
+	// Some helper functions that will be
+	// available in the angular declarations
+
+	$scope.hideTooltip = function(){
+
+		// When a model is changed, the view will be automatically
+		// updated by by AngularJS. In this case it will hide the tooltip.
+
+		$scope.showtooltip = false;
+	}
+
+	$scope.toggleTooltip = function(e){
+		e.stopPropagation();
+		$scope.showtooltip = !$scope.showtooltip;
+	}
+}
+
+function InlineEditorController2($scope){
+
+	// $scope is a special object that makes
+	// its properties available to the view as
+	// variables. Here we set some default values:
+
+	$scope.showtooltip = false;
+  $scope.value2 = 'Enter Name.';
 
 	// Some helper functions that will be
 	// available in the angular declarations
